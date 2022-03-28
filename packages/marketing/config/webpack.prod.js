b9e9368eae2a8d5e3c65e9a3bf697a15e9ecb1ab
cejaythakkar <ce.jaythakkar@gmail.com>
2022-03-28 14:58:33 +0530

    created container deploy script
    created container deploy script

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,9 +1,7 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
-
-const domain = process.env.PRODUCTION_DOMAIN;
+const commonConfig = require("./webpack.common");
 
 const prodConfig = {
   mode: "production",
@@ -12,9 +10,10 @@ const prodConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
-      remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
       },
       shared: packageJson.dependencies,
     }),
